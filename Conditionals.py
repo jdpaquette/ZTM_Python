19728118fa19a6ec1bd87879730aafc0ed723a02
Joseph Paquette <josephdpaquette@gmail.com>
2021-03-16 18:52:34 -0400

    exercise 66 and 70

---
+++ Conditionals.py
@@ -0,0 +1,12 @@
+# Exercise 66 in ZTM_Python
+# Logical Operators
+
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert:
+    print("You are a master magician")
+elif is_magician and not is_expert:
+    print("At least you have magic")
+else:
+    print("You need magical powers!")
